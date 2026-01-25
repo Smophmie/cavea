1363ca2dc97150dd4ec7672537688213b8b69701
Sophie Théréau <sophiethereau@hotmail.fr>
2026-01-25 16:12:34 +0100

    feat (bottle model) : add the relation with grape variety

--- cavea-back/app/Models/Bottle.php
+++ cavea-back/app/Models/Bottle.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Bottle extends Model
 {
@@ -34,5 +36,10 @@ class Bottle extends Model
     public function domain()
     {
         return $this->belongsTo(Domain::class);
+    }
+
+    public function grapeVarieties(): BelongsToMany
+    {
+        return $this->belongsToMany(GrapeVariety::class, 'bottle_grape_varieties');
     }
 }
