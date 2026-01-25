9c230cb93e9f9d6f1e73fac67f309a97d5dec458
Sophie Théréau <sophiethereau@hotmail.fr>
2026-01-25 15:27:21 +0100

    feat (services) : update bottle and comment services

---
+++ cavea-back/app/Services/CommentService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Comment;
+use App\Models\CellarItem;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+
+class CommentService
+{
+
+    public function create(array $data, int $cellarItemId): Comment
+    {
+        Log::info('[COMMENT_SERVICE] Creating comment', [
+            'cellar_item_id' => $cellarItemId,
+            'data' => $data,
+        ]);
+
+        try {
+            $comment = Comment::create([
+                'cellar_item_id' => $cellarItemId,
+                'date' => $data['date'],
+                'content' => $data['content'],
+            ]);
+
+            Log::info('[COMMENT_SERVICE] Comment created successfully', [
+                'comment_id' => $comment->id,
+                'cellar_item_id' => $cellarItemId,
+            ]);
+
+            return $comment;
+        } catch (\Exception $e) {
+            Log::error('[COMMENT_SERVICE] Failed to create comment', [
+                'cellar_item_id' => $cellarItemId,
+                'error_message' => $e->getMessage(),
+                'error_trace' => $e->getTraceAsString(),
+            ]);
+
+            throw $e;
+        }
+    }
+
+    public function delete(Comment $comment): void
+    {
+        $comment->delete();
+    }
+}
