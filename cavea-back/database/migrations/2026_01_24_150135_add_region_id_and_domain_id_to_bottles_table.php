f06c4c0a3700ebe18e3f62f333d1873305793e06
Sophie Théréau <sophiethereau@hotmail.fr>
2026-01-25 15:25:55 +0100

    feat (bottle migration) : region id non nullable

--- cavea-back/database/migrations/2026_01_24_150135_add_region_id_and_domain_id_to_bottles_table.php
+++ cavea-back/database/migrations/2026_01_24_150135_add_region_id_and_domain_id_to_bottles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('bottles', function (Blueprint $table) {
-            $table->foreignId('region_id')->nullable()->constrained('regions');
+            $table->foreignId('region_id')->constrained('regions');
             $table->foreignId('domain_id')->constrained('domains');
         });
     }
