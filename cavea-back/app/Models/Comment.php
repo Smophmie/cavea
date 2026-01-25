2f82758020ab8b55d4f768a3714a4ad5aebf49a1
Sophie Théréau <sophiethereau@hotmail.fr>
2026-01-25 19:07:50 +0100

    feat (comment model) : add the relation with cellar item

--- cavea-back/app/Models/Comment.php
+++ cavea-back/app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
@@ -14,4 +15,9 @@ class Comment extends Model
         'content',
         'cellar_item_id'
         ];
+
+    public function cellarItem(): BelongsTo
+    {
+        return $this->belongsTo(CellarItem::class);
+    }
 }
