6a1ad207a4061ca8fba624ddad92de4d5f905880
Sophie Théréau <sophiethereau@hotmail.fr>
2026-01-25 16:19:35 +0100

    fix (comment model) : fix the class name

--- cavea-back/app/Models/Comment.php
+++ cavea-back/app/Models/Comment.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Appellation extends Model
+class Comment extends Model
 {
     use HasFactory;
 
