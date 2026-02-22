8b9219dcdd037036f6eb071e06906533605ddc63
Sophie Théréau <sophiethereau@hotmail.fr>
2026-02-22 17:13:56 +0100

    style : update style

--- cavea-front/app/loading.tsx
+++ cavea-front/app/loading.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Loading() {
   return (
     <SafeAreaView className="flex-1 bg-app">
-      <View className="items-center justify-center">
+      <View className="flex-1 items-center justify-center">
         <ActivityIndicator size="large" color="#730b1e" />
         <Text className="mt-4">Chargement...</Text>
       </View>
--- cavea-front/app/protected/profile.tsx
+++ cavea-front/app/protected/profile.tsx
@@ -1,21 +1,28 @@
 import { View, Text, ScrollView } from "react-native";
 import PageTitle from "../components/PageTitle";
 import LogoutButton from "../components/LogoutButton";
-import { useAuth } from "@/authentication/AuthContext";
+import SubTitle from "../components/SubTitle";
+import { Image } from "expo-image";
 
 export default function ProfilePage() {
-
-  const { username } = useAuth();
-
   return (
-    <ScrollView 
-      className="flex-1 bg-app px-6"
-      contentContainerStyle={{ justifyContent: "center", alignItems: "center" }}
-    >
+    <ScrollView className="flex-1 bg-app">
+      <View className="w-full bg-wine px-10 py-14">
+        <View className="w-full items-center my-8">
+          <Image
+            source={require("../../assets/images/logo-fond-rouge.png")}
+            style={{ width: "70%", height: 100 }}
+          />
+        </View>
+        <PageTitle text="Mon compte" color="white" />
+      </View>
 
-      <PageTitle text="Mon compte" color="wine"></PageTitle>
-      <LogoutButton></LogoutButton>
-
+      <View className="border border-lightgray rounded-lg p-6 m-6 bg-white">
+        <SubTitle text="Informations" color="black" />
+      </View>
+      <View className="p-6">
+        <LogoutButton />
+      </View>
     </ScrollView>
   );
 }
