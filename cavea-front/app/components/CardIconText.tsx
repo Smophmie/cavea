67d73cbf1e3eac8832c026a42acd7ef6bc5a62b3
Sophie Théréau <sophiethereau@hotmail.fr>
2026-02-24 16:47:26 +0100

    style (card icon text) : add an optional label

--- cavea-front/app/components/CardIconText.tsx
+++ cavea-front/app/components/CardIconText.tsx
@@ -1,25 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ViewStyle } from "react-native";
 import * as LucideIcons from "lucide-react-native";
 
 type CardIconTextProps = {
   text: string;
   icon: keyof typeof LucideIcons;
+  label?: string;
   iconColor?: string;
   textColor?: string;
   backgroundColor?: string;
+  style?: ViewStyle;
 };
 
-export default function CardIconText({ text, icon, iconColor, textColor, backgroundColor }: CardIconTextProps) {
+export default function CardIconText({ text, icon, label, iconColor, textColor, backgroundColor, style }: CardIconTextProps) {
   const IconComponent = LucideIcons[icon] as React.ComponentType<{ size?: number; color?: string }>;
 
   return (
-    <View 
-      className="flex-col items-center gap-2 border border-lightgray rounded-lg px-4 py-3 w-48 h-38"
-      style={{ backgroundColor: backgroundColor || "#ffffff" }}
+    <View
+      className="flex-col items-center gap-2 border border-lightgray rounded-lg px-4 py-3 w-48"
+      style={{ backgroundColor: backgroundColor || "#ffffff", ...style }}
       >
       {IconComponent && <IconComponent size={40} color={iconColor || "#730b1e"} />}
-      <Text className={`text-xl ${textColor || "text-gray"} text-center`}>{text}</Text>
+      {label && <Text className="text-sm text-gray text-center">{label}</Text>}
+      <Text className={`text-xl ${textColor || "text-gray"} text-center font-semibold`}>{text}</Text>
     </View>
   );
 }
