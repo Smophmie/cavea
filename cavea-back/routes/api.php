b8d216f0d897bce1f7fe78720b686e4bbc13eb5b
Sophie Théréau <sophiethereau@hotmail.fr>
2026-02-25 12:15:04 +0100

    refactor (api routes) : delete useless route

--- cavea-back/routes/api.php
+++ cavea-back/routes/api.php
@@ -13,7 +13,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/users/{id}', [UserController::class, 'show']);
-    Route::put('/users/{id}', [UserController::class, 'update']);
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
     Route::get('/user/me', [UserController::class, 'me']);
